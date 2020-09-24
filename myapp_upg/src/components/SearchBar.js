02162479fc1bb06a5f9a1776251f02cb230ce745
ddveloper <44758313+ddveloper@users.noreply.github.com>
2020-09-23 22:01:51 -0700

    refactored

--- myapp_upg/src/components/SearchBar.js
+++ myapp_upg/src/components/SearchBar.js
@@ -1,38 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
-  //   onInputChange(event) {
-  //     // console.log(event.target.value);
-  //   }
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  onFormSubmit = (event) => {
-    // add arrow to fix "this" issue
+  onSubmit = (event) => {
     event.preventDefault();
-    //console.log(this.state.term);
-    this.props.onSubmit(this.state.term); // use this.props to get "props"
+    onFormSubmit(term); // use this.props to get "props"
   };
 
-  render() {
-    // don't put this.onInputChange() instead, pass only reference
-    // this makes sure it got called when needed, not every time rendered
-    // other similar events-handler: onClick / onSubmit, etc.
-    // the handler can also be a lambda (e) => {console.log(e)}
-    return (
-      <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            ></input>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Image Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          ></input>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
