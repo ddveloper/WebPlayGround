69a70425d9fadfeed290c71f97b0bc944fd5ad0f
ddveloper <44758313+ddveloper@users.noreply.github.com>
2020-09-23 21:01:07 -0700

    behind the Route, here are details

--- myhookapp/src/App.js
+++ myhookapp/src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./component/Accordion";
 import Search from "./component/Search";
 import Dropdown from "./component/Dropdown";
 import Translate from "./component/Translate";
+import Route from "./component/Route";
+import Header from "./component/Header";
 
 const items = [
   {
@@ -35,11 +37,27 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
-  return <Search />;
-  return <Accordion items={items} />;
 };
